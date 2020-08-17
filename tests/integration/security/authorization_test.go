d8c716f1a630cb0e328db117a8404402df4efc68
davidraskin <draskin@google.com>
2020-08-17 23:45:58 +0000

    Update integration test + add test to allowlist.txt
    Signed-off-by: davidraskin <draskin@google.com>

--- tests/integration/security/authorization_test.go
+++ tests/integration/security/authorization_test.go
@@ -1053,3 +1053,61 @@ func TestAuthorization_Path(t *testing.T) {
 			rbacUtil.RunRBACTest(t, cases)
 		})
 }
+
+// Test that the AUDIT action does not impact allowing or denying a request
+func TestAuthorization_Audit(t *testing.T) {
+	framework.NewTest(t).
+		Run(func(ctx framework.TestContext) {
+			ns := namespace.NewOrFail(t, ctx, namespace.Config{
+				Prefix: "v1beta1-audit",
+				Inject: true,
+			})
+
+			var a, b, c, d echo.Instance
+			echoboot.NewBuilder(ctx).
+				With(&a, util.EchoConfig("a", ns, false, nil)).
+				With(&b, util.EchoConfig("b", ns, false, nil)).
+				With(&c, util.EchoConfig("c", ns, false, nil)).
+				With(&c, util.EchoConfig("d", ns, false, nil)).
+				BuildOrFail(t)
+
+			newTestCase := func(target echo.Instance, path string, expectAllowed bool) rbacUtil.TestCase {
+				return rbacUtil.TestCase{
+					Request: connection.Checker{
+						From: a,
+						Options: echo.CallOptions{
+							Target:   target,
+							PortName: "http",
+							Scheme:   scheme.HTTP,
+							Path:     path,
+						},
+					},
+					ExpectAllowed: expectAllowed,
+				}
+			}
+			cases := []rbacUtil.TestCase{
+				newTestCase(b, "/allow_audit", true),
+				newTestCase(b, "/other", true),
+				newTestCase(c, "/allow_audit", true),
+				newTestCase(c, "/deny_audit", false),
+				newTestCase(d, "/allow_audit", true),
+				newTestCase(d, "/deny_audit", false),
+			}
+
+			args := map[string]string{
+				"Namespace":     ns.Name(),
+				"RootNamespace": rootNamespace,
+			}
+
+			applyPolicy := func(filename string, ns namespace.Instance) []string {
+				policy := tmpl.EvaluateAllOrFail(t, args, file.AsStringOrFail(t, filename))
+				ctx.Config().ApplyYAMLOrFail(t, ns.Name(), policy...)
+				return policy
+			}
+
+			policy := applyPolicy("testdata/authz/v1beta1-audit.yaml.tmpl", ns)
+			defer ctx.Config().DeleteYAMLOrFail(t, ns.Name(), policy...)
+
+			rbacUtil.RunRBACTest(t, cases)
+		})
+}
